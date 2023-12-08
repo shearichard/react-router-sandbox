38b0db6b8c78e9175a0d4a34c24020539f764d61
shearichard-nec <richard.shea@nec.co.nz>
2023-12-08 16:54:31 +1300

    Remove some now redundant code

--- src/App.js
+++ src/App.js
@@ -50,47 +50,6 @@ function App() {
 			</Router>
 		);
 }
-function AppHIDE() {
-		return (
-			<Router>
-				<div>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/about">About</Link>
-						</li>
-						<li>
-							<Link to="/dashboard">Dashboard(v3)</Link>
-						</li>
-					</ul>
-
-					<hr />
-
-					{/*
-						A <Switch> looks through all its children <Route>
-						elements and renders the first one whose path
-						matches the current URL. Use a <Switch> any time
-						you have multiple routes, but you want only one
-						of them to render at a time
-					*/}
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route path="/about">
-							<About />
-						</Route>
-						<Route path="/dashboard">
-							<Dashboard />
-						</Route>
-					</Switch>
-				</div>
-			</Router>
-		);
-}
-
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
 //

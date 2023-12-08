9ef5e5570d008faef910d6aa595c854c815cfcbc
shearichard-nec <richard.shea@nec.co.nz>
2023-12-08 16:52:48 +1300

    Using the real navbar but not yet set up to route from it

--- src/App.js
+++ src/App.js
@@ -5,9 +5,52 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import NeoNavBarAndSideMenu from './components/NeoNavBarAndSideMenu.js';
 import './App.css';
 
 function App() {
+		const tk="NOT ACTUALLY A TOKEN"
+		return (
+			<Router>
+        <NeoNavBarAndSideMenu tk={tk} />
+				<div>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/dashboard">Dashboard(v3)</Link>
+						</li>
+					</ul>
+
+					<hr />
+
+					{/*
+						A <Switch> looks through all its children <Route>
+						elements and renders the first one whose path
+						matches the current URL. Use a <Switch> any time
+						you have multiple routes, but you want only one
+						of them to render at a time
+					*/}
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/dashboard">
+							<Dashboard />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		);
+}
+function AppHIDE() {
 		return (
 			<Router>
 				<div>
